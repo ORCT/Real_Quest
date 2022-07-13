9381098c72607fb0da5757fbe4a30f7ed4e6c4b0
ORCT <sonic407@naver.com>
2022-07-13 21:36:18 +0900

    Edit GUI Part Add input schedule button Modulize input schedule.py

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import tkinter
 
-root = tkinter.Tk()
+import input_schedule
 
-root.title('Real Quest')
-root.geometry('640x360')
+if __name__ == '__main__':
+    root = tkinter.Tk()
 
-label = tkinter.Label(root, text = 'Schedule')
-label.pack()
+    root.title('Real Quest')
+    root.geometry('640x360')
 
-root.mainloop()
+    label = tkinter.Label(root, text = 'Schedule')
+    label.pack()
+
+    button = tkinter.Button(root, text = 'Input Schedule', overrelief = 'solid', width = 15, command = input_schedule.add_schedule, repeatdelay = 1000, repeatinterval = 100)
+    button.pack()
+
+    root.mainloop()
